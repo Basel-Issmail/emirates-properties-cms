c4b4efb4059f0da0e8c302452b485867625cbeaa
basel.issmail <basel.issmail@gmail.com>
2020-10-28 04:13:30 +0300

    add changes

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -5,7 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions} from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
@@ -24,6 +24,9 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatChipsModule} from '@angular/material/chips';
 import {LayoutModule} from '@angular/cdk/layout';
 
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline'
+};
 
 @NgModule({
   declarations: [],
@@ -77,6 +80,12 @@ import {LayoutModule} from '@angular/cdk/layout';
     MatTooltipModule,
     MatChipsModule,
     LayoutModule
-  ]
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance
+    }
+  ],
 })
 export class MaterialModule { }
